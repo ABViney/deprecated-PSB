079ee8599b89f82e0c1deb4105c55e6d4061de0a
ABViney <vineyadrian@gmail.com>
2023-05-28 18:59:59 -0400

    Set ESR crud to default dashboard view.

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,18 +2,33 @@
 
 namespace App\Controller\Admin;
 
-use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
-use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
-use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use App\Entity\ESR;
+
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
 {
-    #[Route('/admin', name: 'admin')]
+    //
+    #[IsGranted('ROLE_CAN_LOGIN')]
+    #[Route('/dashboard', name: 'dashboard')]
     public function index(): Response
     {
-        return parent::index();
+        // return parent::index();
+        $routeBuilder = $this->container->get(AdminUrlGenerator::class);
+        $url = $routeBuilder->setController(ESRCrudController::class)->generateUrl();
+
+        return $this->redirect($url);
 
         // Option 1. You can make your dashboard redirect to some common page of your backend
         //
@@ -32,6 +47,12 @@ class DashboardController extends AbstractDashboardController
         // return $this->render('some/path/my-dashboard.html.twig');
     }
 
+    public function configureActions(): Actions
+    {
+        return parent::configureActions()
+        ->add(Crud::PAGE_INDEX, Action::DETAIL);
+    }
+
     public function configureDashboard(): Dashboard
     {
         return Dashboard::new()
@@ -40,7 +61,22 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linkToDashboard('Home', 'fa fa-home');
+
+        yield MenuItem::linkToCrud('Equipment Service Reports', 'fa fa-file-contract', ESR::class)
+            ->setAction(Action::INDEX);
+
+        yield MenuItem::section('Settings');
+
+        // If role is Admin then enable:
+        // add/drop employee
+        // change employee permissions
+        // edit all ESR reports
+        // 
+    }
+
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        return parent::configureUserMenu($user);
     }
 }
