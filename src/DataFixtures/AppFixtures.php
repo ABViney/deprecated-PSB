f648b704092908536ed523a65b8a34d531252263
ABViney <vineyadrian@gmail.com>
2023-05-29 17:34:49 -0400

    Refactored. Account fixtures for backend access.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Account;
 use App\Entity\ESR;
 use App\Entity\ESRResult;
 use App\Entity\ESRPartUsed;
@@ -10,24 +11,18 @@ use App\Entity\ESRLabor;
 use App\Entity\Employee;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\PasswordHasherFactoryInterface;
 
 class AppFixtures extends Fixture
 {
+
+    public function __construct(
+        private PasswordHasherFactoryInterface $passwordHasherFactory,
+    )
+    {}
+
     public function load(ObjectManager $manager): void
     {   
-        /**
-         * SELECT esr.serial_no, esrresult.passed, esrpart.description, esrpart_used.quantity, employee.first_name || ' ' || employee.last_name, esrlabor.labor_hours, esr.signed_by
-         * FROM esr
-         * FULL OUTER JOIN esrresult ON esr.esr_result_id=esrresult.id
-         * FULL OUTER JOIN esrpart_used ON esr.id=esrpart_used.esr_id
-         * FULL OUTER JOIN esrpart ON esrpart_used.esr_part_id=esrpart.id
-         * FULL OUTER JOIN esrlabor ON esr.id=esrlabor.esr_id
-         * FULL OUTER JOIN employee on esrlabor.employee_id=employee.id
-         * FULL OUTER JOIN employee on esr.signed_by=employee.id;
-         */
-
-        // Results don't need to be instantiated before the ESR since the
-        // value can be null. It's done here to keep code organized.
         $esr_result_1 = (new ESRResult())
         ->setVisualInspection(true)
         ->setPassed(true);
@@ -50,10 +45,45 @@ class AppFixtures extends Fixture
         $manager->persist($employee_3);
 
 
+        $hasher = $this->passwordHasherFactory->getPasswordHasher(Account::class);
+
+        $account_1 = (new Account())
+        ->setUsername('owner')
+        ->setEmail('owner@psb.com')
+        ->setRoles(['ROLE_ADMIN'])
+        ->setPassword($hasher->hash('man'))
+        ->setAssignedTo($employee_1);
+        $manager->persist($account_1);
+
+        $account_2 = (new Account())
+        ->setUsername('john')
+        ->setEmail('johnmadden@psb.com')
+        ->setRoles(['ROLE_EMPLOYEE'])
+        ->setPassword($hasher->hash('madden'))
+        ->setAssignedTo($employee_2);
+        $manager->persist($account_2);
+
+        $account_3 = (new Account())
+        ->setUsername('steve')
+        ->setEmail('stevejerbs@psb.com')
+        ->setRoles(['ROLE_EMPLOYEE'])
+        ->setPassword($hasher->hash('jerbs'))
+        ->setAssignedTo($employee_3);
+        $manager->persist($account_3);
+
+        $account_4 = (new Account())
+        ->setUsername('admin')
+        ->setEmail('admin@psb.com')
+        ->setRoles(['ROLE_SUPER_ADMIN'])
+        ->setPassword($hasher->hash('admin'))
+        ->setAssignedTo($employee_1);
+        $manager->persist($account_4);
+
         $esr_1 = (new ESR())
         ->setSerialNo('12-12345-123')
         ->setModel('M8')
         ->setDescription('A sample ESR entry')
+        ->setDate(\DateTimeImmutable::createFromFormat('m-d-Y H:i:s', '05-05-2005 11:00:00'))
         ->setEsrResult($esr_result_1)
         ->setProblems('I have to insert entries to test')
         ->setActionTaken('I wrote a fixture to automate this.')
@@ -62,16 +92,22 @@ class AppFixtures extends Fixture
 
 
         $esr_part_1 = (new ESRPart())
-        ->setPartNo('11-222-33-4')
+        ->setPartNo('11-222-EE-4')
         ->setDescription('Thingamabob')
         ->setPrice(50);
         $manager->persist($esr_part_1);
 
         $esr_part_2 = (new ESRPart())
-        ->setPartNo('11-222-33-4')
+        ->setPartNo('86-75-309')
         ->setDescription('Doodad')
         ->setPrice(1500);
         $manager->persist($esr_part_2);
+
+        $esr_part_3 = (new ESRPart())
+        ->setPartNo('51-50-P0P0')
+        ->setDescription('Doohickey')
+        ->setPrice(1499);
+        $manager->persist($esr_part_3);
 
 
         $esr_part_used_1 = (new ESRPartUsed())
@@ -85,6 +121,12 @@ class AppFixtures extends Fixture
         ->setEsrPart($esr_part_2)
         ->setQuantity(1);
         $manager->persist($esr_part_used_2);
+
+        $esr_part_used_3 = (new ESRPartUsed())
+        ->setEsr($esr_1)
+        ->setEsrPart($esr_part_3)
+        ->setQuantity(3);
+        $manager->persist($esr_part_used_3);
 
 
         $esr_labor_1 = (new ESRLabor())
@@ -100,6 +142,7 @@ class AppFixtures extends Fixture
         $manager->persist($esr_labor_2);
         
 
+        
         $manager->flush();
     }
 }
